0f81c61eba2862477bd9eb094712dc36424af8ce
Ricardo <Ricardo@RICARDO.home>
2018-10-22 19:44:22 +0200

    Primeras Pantallas

---
+++ src/main/java/main/MainBuscaminas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author dam2
+ */
+public class MainBuscaminas extends Application {
+    
+     
+    @Override
+    public void start(Stage primaryStage) 
+            throws IOException {
+        
+        FXMLLoader loaderMenu = new FXMLLoader(
+                getClass().getResource("/fxml/FXMLPrincipal.fxml"));
+        BorderPane root = loaderMenu.load();
+ 
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("BUSCAMINAS");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //para no poder maximizar pantalla y
+        primaryStage.setResizable(false);
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/main/java/ramos/controllers/FXMLPantallaPrincipianteController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramos.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ricardo
+ */
+public class FXMLPantallaPrincipianteController implements Initializable {
+    
+    @FXML
+    private Circle btn_cara;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
